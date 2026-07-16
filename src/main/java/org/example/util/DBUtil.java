4df3d560a5d32ab42f34ab1ca7184bc2232a1037
a1l1ke <onetenthplease@gmail.com>
2026-07-16 10:25:33 +0900

    feat: Add DBUtil utility class for database connection

---
+++ src/main/java/org/example/util/DBUtil.java
@@ -0,0 +1,20 @@
+package org.example.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private DBUtil() {} // 생성자 X -> static한 값들만 사용할 예정
+
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        System.out.println("URL = " + URL);
+        System.out.println("USER = " + USER);
+        System.out.println("PASSWORD = " + PASSWORD.substring(0, 5) + "...");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
