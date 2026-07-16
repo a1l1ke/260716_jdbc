b66ea7e5adc751cc75f4d0e4995e5b5ff8640432
a1l1ke <onetenthplease@gmail.com>
2026-07-16 14:29:58 +0900

    feat: Add findById, update, and deleteById workflow to MyCRUD main

--- src/main/java/org/example/step6/MyCRUD.java
+++ src/main/java/org/example/step6/MyCRUD.java
@@ -11,5 +11,16 @@ public class MyCRUD {
         // 제약조건을 어김 (255글자까지인데 400글자 넣기 시도)
 //        repository.save(new Banchan(0, "열무김치".repeat(100)));
         System.out.println("repository.findAll() = " + repository.findAll());
+
+        // 개별 읽기 -> 수정 -> 삭제
+        Banchan myBanchan = repository.findById(1);
+        Banchan myBanchan2 = new Banchan(
+                myBanchan.id(),
+                myBanchan.name() + "라는 이름..."
+        );
+        repository.update(myBanchan2);
+        System.out.println("repository.findAll() = " + repository.findAll());
+        repository.deleteById(1);
+        System.out.println("repository.findAll() = " + repository.findAll());
     }
 }
