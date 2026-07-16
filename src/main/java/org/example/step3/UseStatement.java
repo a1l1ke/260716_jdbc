1170b094fb6d9a3e1a83dfc37862d25477125022
a1l1ke <onetenthplease@gmail.com>
2026-07-16 11:30:12 +0900

    feat: Add example of SQL Injection vulnerability using Statement

--- src/main/java/org/example/step3/UseStatement.java
+++ src/main/java/org/example/step3/UseStatement.java
@@ -9,9 +9,15 @@ import java.sql.Statement;
 
 public class UseStatement {
     public static void main(String[] args) {
+//        String id = "admin";
+//        String id = "admin2";
+        String id = "1234' OR '1'='1";
         try (Connection conn = DBUtil.getConnection()) {
-            String query = "SELECT * FROM users";
-            Statement stmt = conn.createStatement();
+//            String query = "SELECT * FROM users";
+//            String query = "SELECT * FROM users WHERE id = 'admin'";
+            String query = "SELECT * FROM users WHERE id = '%s'".formatted(id);
+            // "SELECT * FROM users WHERE id = '1234' OR '1'='1'"
+            Statement stmt = conn.createStatement(); // SQL Injection의 가능성 때문에 유저의 입력이 있는 부분은 Statement 사용 X
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 // rs -> 컬럼 이름으로 가지고 있음
